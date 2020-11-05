eca1d7625483f71a1273137b56ced3a0a4de8e3c
Mark Baldwin <baldwin.mv05@gmail.com>
2020-11-05 15:40:19 -0700

    Setup Sheet page now connected

---
+++ gitTest/Controllers/SetupSheetController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gitTest.Controllers
+{
+    public class SetupSheetController : Controller
+    {
+        // GET: SetupSheet
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your setup sheet application description page.";
+            return View();
+        }
+    }
+}
