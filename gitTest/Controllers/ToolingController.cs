a3797c7455bc6dfd1db214a00c8fe2102e04e536
Mark Baldwin <baldwin.mv05@gmail.com>
2020-11-06 11:58:49 -0700

    Added more pages, and updated to bootstrap file I wanted

---
+++ gitTest/Controllers/ToolingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gitTest.Controllers
+{
+    public class ToolingController : Controller
+    {
+        // GET: Tooling
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your Tooling description page.";
+            return View();
+        }
+
+        public ActionResult ReOrderList()
+        {
+            ViewBag.Message = "Tooling Re-Order List";
+            return View();
+        }
+    }
+}
---
+++ gitTest/Controllers/TrainingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gitTest.Controllers
+{
+    public class TrainingController : Controller
+    {
+        // GET: Training
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your Training description page.";
+            return View();
+        }
+    }
+}
